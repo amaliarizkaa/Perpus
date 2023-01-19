1c5871718738e6d8dc5106519a3563426b4fc1b2
arthurmorgan2 <nurakura9@gmail.com>
2023-01-20 00:54:52 +0700

    commit

--- public/symlink.php
+++ public/symlink.php
@@ -2,5 +2,5 @@
 
 symlink(
     '/home/https://library.man1yogyakarta.sch.id/portal-berita-man/storage/app/public',
-    '/home/https://library.man1yogyakarta.sch.id/public/storage'
+    '/home/https://library.man1yogyakarta.sch.id/public/uploads'
 );
