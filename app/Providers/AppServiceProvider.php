fa06d7f65c8641cee1d437b10c84859d47f40e41
arthurmorgan2 <nurakura9@gmail.com>
2023-01-20 09:04:58 +0700

    commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,7 +16,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind('path.public', function () {
+            return realpath(public_path() . '/../public');
+        });
     }
 
     /**
