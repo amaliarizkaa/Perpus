a8f16d604ba2b41712a3b9dc8f8f89d7f5b47b06
arthurmorgan2 <nurakura9@gmail.com>
2023-01-20 09:49:16 +0700

    commit

--- public/symlink.php
+++ public/symlink.php
@@ -1,6 +1,6 @@
 <?php
 
 symlink(
-    '/root/portal-berita-man/storage/app/public',
-    '/root/portal-berita-man/public/uploads'
+    '/var/www/html/storage/app/public',
+    '/var/www/html/public/uploads'
 );
